af2503b967e767d324d4ff6c08e9c94e9217aafb
brandonsrho57 <Brandon.Rho.22@cmc.edu>
2021-02-22 23:05:22 -0800

    yes!

--- word_ladder.py
+++ word_ladder.py
@@ -3,12 +3,14 @@
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     '''
-    Returns a list satisfying the following properties:
+    Returns a list satisfying the following
+    properties:
 
     1. the first element is `start_word`
     2. the last element is `end_word`
     3. elements at index i and i+1 are `_adjacent`
-    4. all elements are entries in the `dictionary_file` file
+    4. all elements are entries in the
+    `dictionary_file` file
 
     For example, running the command
     ```
@@ -16,23 +18,65 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     ```
     may give the output
     ```
-    ['stone', 'shone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny', 'boney', 'money']
+    ['stone', 'shone', 'phone', 'phony',
+    'peony', 'penny', 'benny', 'bonny',
+    'boney', 'money']
     ```
     but the possible outputs are not unique,
     so you may also get the output
     ```
-    ['stone', 'shone', 'shote', 'shots', 'soots', 'hoots', 'hooty', 'hooey', 'honey', 'money']
+    ['stone', 'shone', 'shote', 'shots',
+    'soots', 'hoots', 'hooty', 'hooey',
+    'honey', 'money']
     ```
-    (We cannot use doctests here because the outputs are not unique.)
+    (We cannot use doctests here because the
+    outputs are not unique.)
 
-    Whenever it is impossible to generate a word ladder between the two words,
+    Whenever it is impossible to generate a
+    word ladder between the two words,
     the function returns `None`.
     '''
+    from collections import deque
+    import copy
+
+    with open(dictionary_file) as f:
+        hard_dictionary = f.readlines()
+
+    dictionary = []
+
+    for definitions in hard_dictionary:
+        dictionary.append(definitions.strip())
+
+    if start_word == end_word:
+        return [start_word]
+    if len(start_word) != len(end_word):
+        return None
+    if _adjacent(start_word, end_word):
+        return [start_word, end_word]
+    stack = []
+    stack.append(start_word)
+    dictionary.remove(start_word)
+    queue = deque([])
+    queue.append(stack)
+
+    while len(queue) != 0:
+        stack2 = queue.popleft()
+        dictionary_copy = copy.deepcopy(dictionary)
+        for words in dictionary_copy:
+            if _adjacent(stack2[-1], words):
+                if words == end_word:
+                    stack2.append(words)
+                    return stack2
+                new_stack = copy.deepcopy(stack2)
+                new_stack.append(words)
+                queue.append(new_stack)
+                dictionary.remove(words)
 
 
 def verify_word_ladder(ladder):
     '''
-    Returns True if each entry of the input list is adjacent to its neighbors;
+    Returns True if each entry of the input list
+    is adjacent to its neighbors;
     otherwise returns False.
 
     >>> verify_word_ladder(['stone', 'shone', 'phone', 'phony'])
@@ -40,6 +84,12 @@ def verify_word_ladder(ladder):
     >>> verify_word_ladder(['stone', 'shone', 'phony'])
     False
     '''
+    if len(ladder) < 1:
+        return False
+    for i in range(0, len(ladder) - 1):
+        if not _adjacent(ladder[i], ladder[i + 1]):
+            return False
+    return True
 
 
 def _adjacent(word1, word2):
@@ -52,3 +102,10 @@ def _adjacent(word1, word2):
     >>> _adjacent('stone','money')
     False
     '''
+    if len(word1) != len(word2):
+        return False
+    counter = 0
+    for i in range(0, min(len(word1), len(word2))):
+        if word1[i] != word2[i]:
+            counter += 1
+    return counter == 1
